a509ffc4a879770457a1ece7a504ff9cdd2e90cd
Pavel <recyclesordie94@gmail.com>
2021-08-19 22:57:43 +0300

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <React.Fragment>
+<header>
+            <h1>Reddit for own %))</h1>
+        </header>
+        </React.Fragment>
+    )
+}
+
+export default Header;
